264358bfd65891ada79d9c309add2cf7c2524a52
GuediaDavid <guediadavid@yahoo.com>
2019-12-26 09:38:50 +0100

    mon projet

---
+++ ontologie/src/main/java/com/ontologie/ontologie/OntologieApplication.java
@@ -0,0 +1,13 @@
+package com.ontologie.ontologie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntologieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OntologieApplication.class, args);
+	}
+
+}
---
+++ ontologie/src/test/java/com/ontologie/ontologie/OntologieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ontologie.ontologie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OntologieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
